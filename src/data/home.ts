1229109efee81c707eb1d1d5f782e25d3662fbd1
Decode <amitk.mca21@rvce.edu.in>
2023-11-24 10:31:30 +0530

    First Commit

---
+++ src/data/home.ts
@@ -0,0 +1,119 @@
+export const categories = [
+    "All",
+    "JavaScript",
+    "TypeScript",
+    "Programming",
+    "Travelling",
+    "Cricket World Cup 2023",
+    "Bowling",
+    "Cricket",
+    "React",
+    "Next.js",
+    "UI/UX Design",
+    "Object Oriented Programming",
+    "Frontend Web Development",
+    "Backend Web Development",
+    "Web Development",
+    "Coding",
+]
+
+export const videos = [
+    {
+        id: "1",
+        title: "How to get ahead of 99% of people",
+        channel: {
+            name: "Ruri Ohama",
+            id: "Ruri Ohama",
+            profileUrl:
+                "https://yt3.googleusercontent.com/gw6BRWav3SyG39C2kmEM1VSb5ocjEWuKRBKPhRndqOKmAxj3rzB5OOVQKeE0751DJrPWJH7c=s900-c-k-c0x00ffffff-no-rj",
+        },
+        views: 777777,
+        postedAt: new Date("2023-9-08"),
+        duration: 1117,
+        thumbnailUrl: "https://i3.ytimg.com/vi/p_0MaEUBMYg/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "2",
+        title: "Sia - Unstoppable (Official Audio)",
+        channel: {
+            name: "Sia",
+            id: "Sia",
+            profileUrl:
+                "https://yt3.ggpht.com/4cFOo3R_D4gOVVJPEt8aoff-Fi2eRjM5Nxz5fDXpw0WS57go_buPi0kvhehvTnBZeF3qSmAT=s900-c-k-c0x00ffffff-no-nd-rj",
+        },
+        views: 172464814,
+        postedAt: new Date("2016-1-21"),
+        duration: 217,
+        thumbnailUrl: "https://i3.ytimg.com/vi/cxjvTXo9WWM/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "3",
+        title: "THE BATMAN – Main Trailer",
+        channel: {
+            name: "Warner Bros. Pictures",
+            id: "Warner Bros. Pictures",
+            profileUrl:
+                "https://yt3.googleusercontent.com/CoYiDB8ojHTQvSwr9h8clzpcm85wiFaHroW0MaVgTnDEXAIMxhGlzC5Vwo9N3-z3obfVqc_G62I=s900-c-k-c0x00ffffff-no-rj",
+        },
+        views: 60000000,
+        postedAt: new Date("2021-11-02"),
+        duration: 1000,
+        thumbnailUrl: "https://i3.ytimg.com/vi/mqqft2x_Aa4/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "4",
+        title: "Marvel Studios' Avengers: Endgame - Official Trailer",
+        channel: {
+            name: "Marvel Entertainment",
+            id: "Marvel Entertainment",
+            profileUrl:
+                "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+        },
+        views: 103400000,
+        postedAt: new Date("2020-03-03"),
+        duration: 126,
+        thumbnailUrl: "https://i3.ytimg.com/vi/TcMBFSGVi1c/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+  
+    {
+        id: "5",
+        title: "Hidden Details in Loki's Costumes! | Behind the Scenes",
+        channel: {
+            name: "Marvel Entertainment",
+            id: "Marvel Entertainment",
+            profileUrl:
+                "https://yt3.googleusercontent.com/ugAmG9LeliJJoiyacIecdiq_ZgRNdmjCIohaN5x3QEOmWB9dNUsKuCU8ngLs3JUauHZ4-boVkA=s800-c-k-c0x00ffffff-no-rj",
+        },
+        views: 20323340,
+        postedAt: new Date("2023-11-17"),
+        duration: 307,
+        thumbnailUrl: "https://i3.ytimg.com/vi/mS9nfsA-oGI/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+
+    {
+        id: "6",
+        title: "Peaky Blinders Season 6 Official Trailer | Netflix India",
+        channel: {
+            name: "Netflix India",
+            id: "Netflix India",
+            profileUrl:
+                "https://yt3.googleusercontent.com/zgMN9BuSQByG1SrpmLwcNB3MQhjDhS_pl9H1h7TaRievMfS4UpU7Z36j77z5_hnIW4N8uFX3NA=s900-c-k-c0x00ffffff-no-rj",
+        },
+        views: 5323,
+        postedAt: new Date("2023-8-17"),
+        duration: 93,
+        thumbnailUrl: "https://i3.ytimg.com/vi/PxZ5gGfPtCQ/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    }
+]
